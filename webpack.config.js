2e9670a3756c7470772c2b07cfb5b17a3c39c03a
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-14 17:02:44 -0400

    same

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(png|jpg)$/i,
+                test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
                         loader: 'file-loader',
